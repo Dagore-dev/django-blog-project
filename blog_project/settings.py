c290e983e175e7a15423514824806d976af463b6
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2023-02-14 12:32:14 +0100

    accounts app

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog_list'
+LOGOUT_REDIRECT_URL = 'blog_list'
